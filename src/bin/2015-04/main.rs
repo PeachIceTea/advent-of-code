97f205f12b7d13f2dbef4e4fdf2df17b856cfdff
Jonas Thiem <jonthiem@gmail.com>
2022-10-14 08:19:59 +0200

    Initial commit
    Solutions for 2015 days 1 through 6 in Rust.

---
+++ src/bin/2015-04/main.rs
@@ -0,0 +1,28 @@
+fn create_zero_string(num_of_zeroes: i32) -> String {
+    let mut s = String::new();
+    for _ in 0..num_of_zeroes {
+        s.push('0');
+    }
+    s
+}
+
+fn find_advent_coin_num(secret_key: &str, num_of_zeroes: i32) -> (String, i32) {
+    let starting_zeroes = create_zero_string(num_of_zeroes);
+    let mut current_num = 1;
+    loop {
+        let hash = format!("{:x}", md5::compute(format!("{secret_key}{current_num}")));
+        if hash.starts_with(starting_zeroes.as_str()) {
+            break (hash, current_num);
+        }
+        current_num += 1;
+    }
+}
+
+fn main() {
+    let secret_key = std::fs::read_to_string("input/day4/input.txt")
+        .expect("input file should be at input/day4/input.txt");
+
+    let (hash5, num5) = find_advent_coin_num(secret_key.as_str(), 5);
+    let (hash6, num6) = find_advent_coin_num(secret_key.as_str(), 6);
+    println!("For the secret \"{}\" the first number to create a hash with 5 leading zeroes is {} creating the hash {} and with 6 leading zeroes the number is {} creating the hash {}.", secret_key, num5, hash5, num6, hash6);
+}
---
+++ src/bin/2015-05/main.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = std::fs::read_to_string("input/day5/input.txt")
+        .expect("input file should be at input/day5/input.txt");
+
+    // First part. I struggled with this quite a bit. This implementation is not as good as it could
+    // be but I do not know what was wrong with my first one :<
+    let vowel_list = ['a', 'e', 'i', 'o', 'u'];
+    let naughty_list = ["ab", "cd", "pq", "xy"];
+
+    let mut nice_string_count = 0;
+    'line: for line in input.lines() {
+        let mut vowels = 0;
+        let mut has_double_letter = false;
+
+        let mut prev_letter = ' ';
+        for letter in line.chars() {
+            let combo = format!("{prev_letter}{letter}");
+            if naughty_list.contains(&combo.as_str()) {
+                continue 'line;
+            }
+
+            if vowel_list.contains(&letter) {
+                vowels += 1;
+            }
+
+            if letter == prev_letter {
+                has_double_letter = true;
+            }
+
+            prev_letter = letter
+        }
+
+        if vowels >= 3 && has_double_letter {
+            nice_string_count += 1;
+        }
+    }
+
+    // Second part. Can you tell I kinda gave up on making this nice?
+    let mut nice_string_count2 = 0;
+    for line in input.lines() {
+        let mut has_non_overlapping_pair = false;
+        let mut has_repeating_letter_with_one_in_between = false;
+
+        let mut pairs = HashMap::new();
+        let mut prev_prev_letter = ' ';
+        let mut prev_letter = ' ';
+        for letter in line.chars() {
+            if !(prev_letter == letter && prev_prev_letter == letter) {
+                let combo = format!("{prev_letter}{letter}");
+                pairs
+                    .entry(combo)
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+
+            if prev_prev_letter == letter {
+                has_repeating_letter_with_one_in_between = true;
+            }
+
+            prev_prev_letter = prev_letter;
+            prev_letter = letter;
+        }
+
+        for pair_count in pairs.values() {
+            if *pair_count >= 2 {
+                has_non_overlapping_pair = true;
+                break;
+            }
+        }
+
+        if has_non_overlapping_pair && has_repeating_letter_with_one_in_between {
+            nice_string_count2 += 1;
+        }
+    }
+
+    println!("Using method 1 there are {nice_string_count} nice strings on Santa's text file and with method 2 there are {nice_string_count2} nice strings.");
+}
